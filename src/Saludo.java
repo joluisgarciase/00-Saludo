c1b9acf26bdca6ab68eadbbcedb2d9a562b47cd6
joluisgarciase <deadman94_18@hotmail.com>
2016-01-15 14:11:50 -0600

    00-Saludo

---
+++ src/Saludo.java
@@ -0,0 +1,22 @@
+import javax.swing.JOptionPane;
+/**
+ * 
+ * @author Luis Garcia
+ * @version 0.0.1
+ * Clase principal la cual mandara un mensaje.
+ *
+ */
+public class Saludo {
+	
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "José Luis");
+		System.out.print("Garcia Serrano");
+
+	}
+
+}
